b26a30864924564057168a4b156ba7ea54517cad
duan.sd <dxddshd@126.com>
2016-05-31 23:40:58 +0800

    代码整理

--- node_modules/avalon2/components/button/index.js
+++
@@ -1,9 +0,0 @@
-//var avalon = require('avalon')
-
-avalon.component('ms-button', {
-    template: '<button type="button"><span><slot name="buttonText"></slot></span></button>',
-    defaults: {
-        buttonText: "button"
-    },
-    soleSlot: 'buttonText'
-})
